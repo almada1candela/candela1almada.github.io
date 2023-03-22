819a370ed2dc9bb7f6eaf4138ccf74f5076fee49
almada1candela <125842730+almada1candela@users.noreply.github.com>
2023-03-22 11:56:24 -0300

    initial commit

---
+++ function.js
@@ -0,0 +1,7 @@
+document.getElementById('boton_color').addEventListener('click', function(){
+    document.body.style.backgroundColor='#a8ede9a1';
+});
+
+document.getElementById('boton_default').addEventListener('click', function(){
+    document.body.style.backgroundColor='#FFF8F0';
+});
